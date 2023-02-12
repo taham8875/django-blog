ead2d24c129d69a874bd408df286f61ab384ee2e
Taha Ahmed <taha.m8875@gmail.com>
2023-02-12 15:16:09 +0200

    Part 3 - Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,10 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Taha',
+        'title': 'blog post 1',
+        'content': 'hello world',
+        'date': '2023'
+    },
+    {
+        'author': 'Omar',
+        'title': 'blog post 2',
+        'content': 'hello world 2',
+        'date': '2013'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.jinja2', context={'posts': posts})
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.jinja2')
