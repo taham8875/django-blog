fb2cc53061058d9dd4ca9efded9606713c9ca7e3
Taha Ahmed <taha.m8875@gmail.com>
2023-02-13 10:26:17 +0200

    Part 5 - Database and Migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from blog.models import Post
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Taha',
-        'title': 'blog post 1',
-        'content': 'hello world',
-        'date': '2023'
-    },
-    {
-        'author': 'Omar',
-        'title': 'blog post 2',
-        'content': 'hello world 2',
-        'date': '2013'
-    }
-]
 
 
 def home(request):
-    return render(request, 'blog/home.jinja2', context={'posts': posts})
+    return render(request, 'blog/home.jinja2', context={'posts': Post.objects.all()})
 
 
 def about(request):
-    return render(request, 'blog/about.jinja2')
+    return render(request, 'blog/about.jinja2', context={'title': 'about'})
